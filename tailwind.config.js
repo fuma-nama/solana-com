a7c15e58d585959da839c1cf87cf86135204aae6
Fuma Nama <god63820869@gmail.com>
2025-02-24 17:35:43 +0800

    update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
-import { createPreset } from "fumadocs-ui/tailwind-plugin";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
-  presets: [createPreset()],
   content: [
     "./node_modules/fumadocs-ui/dist/**/*.js",
     // "./src/components/**/*.{ts,tsx}",
